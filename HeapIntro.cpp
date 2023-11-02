00d1ada39ad66bb9ef57a7ea6e1ffc3a116a50ce
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2023-11-02 23:43:48 +0530

    Update HeapIntro.cpp

--- HeapIntro.cpp
+++ HeapIntro.cpp
@@ -1,1 +1,19 @@
+void HeapInsert(int *arr,int size,int x){
+    size+=1;
 
+    //put the node at end
+    arr[size]=x;
+
+    //put at right position
+    int left=2*size;
+    int right=2*size+1;
+
+    if(left<size && )
+
+}
+int main(){
+    int size=0;
+    int arr[100];
+    HeapInsert(arr,size,5);
+    
+}
