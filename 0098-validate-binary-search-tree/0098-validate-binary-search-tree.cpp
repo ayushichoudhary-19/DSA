b515b0153c0cbd56adb4517ad56927f9d10afddd
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2024-01-18 01:45:04 +0530

    Time: 14 ms (20.83%), Space: 22 MB (88.70%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+private:
+    bool validChecker(TreeNode* root, long long low, long long high){
+        if(root==nullptr) return true;
+        
+        if(root->val>=high || root->val<=low) return false;
+        
+        bool left,right;
+        //check left and right subtree now that current node is correct 
+        if(root->left)
+                left=validChecker(root->left,low,root->val);
+                if(!left) return false;
+        else left=true;
+            
+        if(root->right)
+               right=validChecker(root->right,root->val,high); 
+        else right =true;
+        
+        return left&&right;
+    }
+public:
+    bool isValidBST(TreeNode* root) {
+        if(root==nullptr) return true;
+         
+        //leaf node
+        if(!root->left && !root->right) return true;
+        
+        //Starting range is [int_min to int_max]
+        long long low = LLONG_MIN, high = LLONG_MAX;
+       
+        return validChecker(root,low,high);  
+}
+};
