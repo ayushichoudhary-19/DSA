89fea979f51ef15db215412bf8c78372aa89c330
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2024-01-31 09:39:03 +0530

    Time: 117 ms (92.62%), Space: 100.9 MB (68.65%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,22 +1,20 @@
+using namespace std;
+
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        int size=temperatures.size();
-        vector<int> answer(size,0);
-        stack<pair<int,int>>st;
+        int size = temperatures.size();
+        vector<int> answer(size, 0);
+        stack<int> st;
 
-        for(int i=size-1;i>=0;i--){
-            while(!st.empty() && st.top().first<=temperatures[i]){
+        for (int i = size - 1; i >= 0; i--) {
+            while (!st.empty() && temperatures[st.top()] <= temperatures[i]) {
                 st.pop();
             }
-            // now top most is greater than the current element
-            if(!st.empty()){
-                answer[i]=st.top().second-i;
+            if (!st.empty()) {
+                answer[i] = st.top() - i;
             }
-            else{
-                answer[i]=0;
-            }
-            st.push({temperatures[i],i});
+            st.push(i);
         }
         return answer;
     }
