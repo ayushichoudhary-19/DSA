ddde73e9285789c30db15179b3008a7e7eec8126
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2024-07-05 03:13:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    void NSL(vector<int> &nsl, vector<int>& heights){
+        int size=heights.size();
+        stack<int> st;
+        for(int i=0; i<size; i++){
+            while(!st.empty() && heights[st.top()]>heights[i]){
+                st.pop();
+            }
+            if(st.empty()) nsl[i]=-1;
+            else nsl[i]=st.top();
+            
+            st.push(i);
+        }
+    }
+
+    void NSR(vector<int> &nsr, vector<int>& heights){
+        int size=heights.size();
+        stack<int> st;
+        for(int i=size-1; i>=0; i--){
+            while(!st.empty() && heights[st.top()]>heights[i]){
+                st.pop();
+            }
+            if(st.empty()) nsr[i]=-1;
+            else nsr[i]=st.top();
+            
+            st.push(i);
+        }
+    }
+
+    int largestRectangleArea(vector<int>& heights) {
+        int size=heights.size();
+        if(size==1) return heights[0];
+
+        vector<int> nsl(size,-1);
+        vector<int> nsr(size,size);
+        NSR(nsr,heights);
+        NSL(nsl,heights);
+        
+        int maxArea=0;
+        for(int i=0; i<size; i++){
+            int currArea=heights[i] * (nsr[i]-nsl[i]-1);
+            cout<<currArea<<" ";
+            maxArea=max(maxArea,currArea);
+        }
+        return maxArea;
+        
+    }
+};
