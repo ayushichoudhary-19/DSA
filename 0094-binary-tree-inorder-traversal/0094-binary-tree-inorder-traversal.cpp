9d23da673f87a88f8f0a4b3780cd67b8a6f0f201
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2024-07-08 18:40:46 +0530

    Time: 0 ms (100%), Space: 9.2 MB (99.98%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,24 +11,40 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int> &ans){
-
-        //base case
-        if(root==nullptr) return;
-
-        inorder(root->left, ans);
-
-        ans.push_back(root->val);
-
-        inorder(root->right, ans);
-
-    }
     vector<int> inorderTraversal(TreeNode* root) {
         
         //left -> root-> right
 
         vector<int> ans;
-        inorder(root, ans);
+        
+        TreeNode* curr=root;
+        while(curr!=NULL){
+            //at every root node, check its left subtree's rightmost node and connect it to the root
+
+            if(curr->left==NULL){
+                //if no left subtree is there then store this 
+                //as inorder is left, root, right. with no left, write the root
+                ans.push_back(curr->val);
+                //move to the right subtree
+                curr=curr->right;
+            }
+            else{
+                //left subtree exsists, go to its rightmost node
+                TreeNode* leftChild=curr->left;
+                //get to the leftmost node of this subtree whose root node is leftchild node
+                while(leftChild->right!=NULL){
+                    leftChild=leftChild->right;
+                }
+                //now connect the rightmost to the parent root which is pointed by curr
+                leftChild->right=curr;
+
+                //now to make sure already traversed left subtree is never traversed back, make it null
+                TreeNode* temp = curr;
+                curr = curr -> left;
+                temp -> left = NULL;
+            }
+
+        }
         return ans;
 
     }
