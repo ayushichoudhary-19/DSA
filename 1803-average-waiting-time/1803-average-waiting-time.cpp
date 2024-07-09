ca86e3f84416e486b7f60d1b42f7f02cdf61c29c
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2024-07-09 12:10:15 +0530

    Time: 172 ms (25.29%), Space: 86.5 MB (6.47%) - LeetHub

---
+++ 1803-average-waiting-time/1803-average-waiting-time.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        double ans=0;
+
+        int currTime=customers[0][0];
+        for(auto order: customers){
+            currTime=max(currTime,order[0]);
+            int freeAt = currTime + order[1];
+            currTime = freeAt;
+            ans = ans + (currTime - order[0]);
+        }
+        return ans/customers.size();
+    }
+};
