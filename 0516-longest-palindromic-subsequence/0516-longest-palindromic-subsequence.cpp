f13acf21218c6b6d5fba14cdc6d3836094e24b3b
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2024-07-19 00:10:55 +0530

    Time: 98 ms (9.15%), Space: 74.7 MB (5.45%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -12,10 +12,10 @@ public:
         vector<vector<int>> dp(n+1,vector<int> (n+1,0));
         
         //base case
-        for(int i=0; i<=n; i++){
-            dp[i][0]=0;
-            dp[0][i]=0;    
-        }
+        // for(int i=0; i<=n; i++){
+        //     dp[i][0]=0;
+        //     dp[0][i]=0;    
+        // }
 
         for(int i=1; i<=n; i++){
             for(int j=1; j<=n; j++){
