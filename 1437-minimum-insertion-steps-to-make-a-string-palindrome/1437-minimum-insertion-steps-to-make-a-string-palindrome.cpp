c9a329f1ad4f46993a783e02e873eebe5a28d965
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2024-07-19 00:23:20 +0530

    Time: 39 ms (21.75%), Space: 30.2 MB (5.15%) - LeetHub

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    int longestPalindromeSubseq(string s) {
+        int n=s.length();
+        string s1=s;
+        //for not changing input string, we do not use rev fun but do manually
+        string s2;
+        for(int i=n-1; i>=0; i--){
+            s2+=s[i];
+        }
+
+        vector<vector<int>> dp(n+1,vector<int> (n+1,0));
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(s1[i-1]==s2[j-1]) dp[i][j] = dp[i-1][j-1] + 1;
+                else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[n][n];
+    }
+public:
+//find len - longest palindromic 
+    int minInsertions(string s) {
+        int len=s.length();
+        int palinLen = longestPalindromeSubseq(s);
+        return len - palinLen;
+    }
+};
