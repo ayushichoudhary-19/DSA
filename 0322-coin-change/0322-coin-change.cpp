6b11ff59da9b1f816ce2bff0503de4fe09392fdf
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2024-07-18 19:34:30 +0530

    Time: 40 ms (49%), Space: 20.6 MB (48.75%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,26 +2,27 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount+1,0));
+        vector<int> prev(amount+1,0), curr(amount+1,0);
 
         //base case
        for(int target=0; target<=amount;  target++){
         if(target%coins[0]==0)
-            dp[0][target] = target/coins[0];
-        else dp[0][target] = 1e9;
+            prev[target] = target/coins[0];
+        else prev[target] = 1e9;
         }
 
         for(int idx=1; idx<n; idx++){
             for(int target=0; target<=amount; target++){
                 int pick = 1e9;
-                if(target>=coins[idx]) pick = 1 + dp[idx][target-coins[idx]];
-                int nonpick = 0 + dp[idx-1][target];
+                if(target>=coins[idx]) pick = 1 + curr[target-coins[idx]];
+                int nonpick = 0 + prev[target];
 
-                dp[idx][target]=min(pick,nonpick);
+                curr[target]=min(pick,nonpick);
             }
+            prev=curr;
         }
        
-        int ans= dp[n-1][amount];
+        int ans= prev[amount];
 
         if(ans >= 1e9)
         return -1;
