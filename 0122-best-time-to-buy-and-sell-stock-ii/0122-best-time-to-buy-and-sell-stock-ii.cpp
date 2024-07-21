00fef7ff8348e86c855c9001ba7e860f78a5cac3
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2024-07-21 12:19:37 +0530

    Time: 11 ms (18.85%), Space: 18.1 MB (20.86%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,26 +1,27 @@
 class Solution {
-private:
-    int solve(int idx, int buy, vector<int>& prices, vector<vector<int>> &dp){
-        if(idx==prices.size()) return 0;
-
-        if(dp[idx][buy]!=-1) return dp[idx][buy];
-
-
-        int prof1=0, prof2=0;
-        //buy available
-        if(buy) {   
-                    //buy today.    //skip buying
-            prof1 = max(-prices[idx] + solve(idx+1, 0, prices,dp),  0 + solve(idx+1,1,prices,dp));
-        }
-        else{
-            prof2= max(prices[idx] + solve(idx+1,1,prices,dp), 0 + solve(idx+1,0,prices,dp));
-        }
-        return dp[idx][buy] = max(prof1,prof2);
-    }
 public:
     int maxProfit(vector<int>& prices) {
         int days=prices.size();
-        vector<vector<int>> dp(days, vector<int> (2,-1));
-        return solve(0,1,prices,dp);
+        vector<vector<int>> dp(days+1, vector<int> (2,-1));
+
+        //base case
+        dp[days][0] = 0;
+        dp[days][1] = 0;
+
+        for(int idx=days-1; idx>=0; idx--){
+            for(int buy = 0; buy<2; buy++){
+                int prof1=0, prof2=0;
+                    //buy available
+                    if(buy) {   
+                    //buy today.        //skip buying
+                         prof1 = max(-prices[idx] + dp[idx+1][0],  0 + dp[idx+1][1]);
+                    }
+                    else{
+                        prof2= max(prices[idx] + dp[idx+1][1], 0 + dp[idx+1][0]);
+                    }
+                    dp[idx][buy] = max(prof1,prof2);
+            }
+        }
+        return dp[0][1];
     }
 };
