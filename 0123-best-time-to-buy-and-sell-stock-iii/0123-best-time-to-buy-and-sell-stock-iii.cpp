50a26ca35b58d40cac60f492551e4bd1ebd70e81
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2024-07-21 18:23:36 +0530

    Time: 430 ms (47.95%), Space: 210.1 MB (37.68%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,25 +1,27 @@
 class Solution {
-private:
-    int solve(int idx, int buy, int cap, vector<int> &prices, vector<vector<vector<int>>> &dp){
-        //base case
-        if(cap==0) return 0;
-        if(idx==prices.size()) return 0;
-        if (dp[idx][buy][cap] != -1) return dp[idx][buy][cap];
-
-        int buythis = 0, sellthis=0;
-        if(buy){
-            buythis = max(-prices[idx] + solve(idx+1, 0, cap, prices,dp), 0 + solve(idx+1, 1, cap, prices,dp));
-        }
-        else{
-            sellthis = max(prices[idx] + solve(idx+1, 1, cap-1, prices,dp), 0 + solve(idx+1, 0, cap, prices,dp));
-        }
-        return dp[idx][buy][cap] = max(sellthis,buythis);
-    }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
         
-        return solve(0, 1, 2, prices,dp);
-    }
+        //base case
+        for(int i=0; i<n; i++){
+            dp[i][0][0] = 0;
+            dp[i][1][0] = 0;
+        }
+
+
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int buy = 0; buy < 2; buy++) {
+                for (int cap = 1; cap < 3; cap++) {
+                    if (buy) {
+                        dp[idx][buy][cap] = max(-prices[idx] + dp[idx + 1][0][cap], dp[idx + 1][1][cap]);
+                    } else {
+                        dp[idx][buy][cap] = max(prices[idx] + dp[idx + 1][1][cap - 1], dp[idx + 1][0][cap]);
+                    }
+                }
+            }
+        }
+
+        return dp[0][1][2];}
 };
