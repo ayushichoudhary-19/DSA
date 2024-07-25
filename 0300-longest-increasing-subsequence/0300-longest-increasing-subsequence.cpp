ef96eb7471d93a7a572fe8f3a00a6fa6bad93f22
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2024-07-25 17:39:14 +0530

    Time: 94 ms (50.74%), Space: 13.3 MB (44.35%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        
-        // We use n+1 to handle the case where idx is n and prev is -1
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
-
-        for (int idx = n - 1; idx >= 0; idx--) {
-            for (int prev = idx - 1; prev >= -1; prev--) {
-                int take = 0;
-                if (prev == -1 || nums[prev] < nums[idx]) {
-                    take = 1 + dp[idx + 1][idx + 1];
+        int n=nums.size();
+        vector<int> dp(n,1), hash(n);
+        int maxi=INT_MIN;
+        for(int idx=0; idx<n; idx++){
+            for(int prev=0; prev<idx; prev++){
+                if(nums[prev]< nums[idx]){
+                    dp[idx] = max(dp[idx], 1 + dp[prev]);
                 }
-                int nontake = dp[idx + 1][prev + 1];
-
-                dp[idx][prev + 1] = max(take, nontake);
             }
+            maxi = max(maxi, dp[idx]);
         }
-        
-        // Return the result for the case where idx is 0 and prev is -1
-        return dp[0][0];
+        return maxi;
     }
 };
