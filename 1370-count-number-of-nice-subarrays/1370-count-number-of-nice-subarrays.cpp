152c9b0d7e30006893292fdcbb22c072592d4a08
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2024-07-26 20:31:55 +0530

    Time: 95 ms (53.81%), Space: 70 MB (79.64%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -3,27 +3,31 @@ private:
     int helper(vector<int>& nums, int k) {
         if(k<0) return 0;
         int l=0, r=0, n=nums.size();
-        int oddcount=0;
-        int ans=0;
+        int sum=0;
+        int count=0;
 
         while(r<n){
-            if(nums[r]%2==1){
-                oddcount++;
-            }
-            while(oddcount > k){
-                if(nums[l] % 2 == 1) {
-                    oddcount--;
-                }
+            sum+=nums[r];
+            while(sum > k){
+                sum-=nums[l];
                 l++;
             }
-            ans += (r-l+1);
+            count += (r-l+1);
             r++;
         }
-        return ans;
+        return count;
     }
 
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]%2==1){
+                nums[i]=1;
+            }
+            else nums[i]=0;
+        }
+
+        //now rest is same as: no. of binary subarrays with given sum
         return helper(nums, k) - helper(nums, k-1);
     }
 };
