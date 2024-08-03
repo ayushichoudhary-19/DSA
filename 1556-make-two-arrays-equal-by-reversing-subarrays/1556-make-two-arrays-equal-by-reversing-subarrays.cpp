3f6e4a0e41c6f1e74b7cbd3708444d38c1050d5f
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2024-08-03 18:39:26 +0530

    Time: 3 ms (98.33%), Space: 17.3 MB (62.74%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -4,15 +4,11 @@ public:
         int n1=target.size(), n2=arr.size();
         if(n1!=n2) return false;
 
-        unordered_map<int,int> map;
-        for(int i=0; i<n2; i++){
-            map[arr[i]]++;
-        }
+        sort(target.begin(),target.end());
+        sort(arr.begin(),arr.end());
 
         for(int i=0; i<n1; i++){
-            if(map.find(target[i])==map.end()) return false;
-            map[target[i]]--;
-            if(map[target[i]]==0) map.erase(target[i]);
+            if(arr[i]!=target[i]) return false;
         }
         return true;
     }
