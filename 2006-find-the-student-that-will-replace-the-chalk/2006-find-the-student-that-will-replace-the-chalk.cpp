94c069e6fde385050e97c4fda9daedce3a226d8a
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2024-09-02 14:45:58 +0530

    Time: 106 ms (17.67%), Space: 77.1 MB (8.71%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
@@ -2,17 +2,21 @@ class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
         long long sum=0;
-        for(auto ch:chalk){
-            sum+=ch;
-        }
-        // chalksLeftForDecisingRound
-        k=k%sum;
-        for(int stu=0; stu<chalk.size(); stu++){
-            if(chalk[stu]<=k) {
-                    k-=chalk[stu];
-                }
-                else return stu;
-        }
-        return -1;
+        for(auto& ch:chalk) sum+=ch;
+
+        //fine rounds where k lasts for all
+        long long f = k/sum;
+
+        //now whatever is left for that sum, we need to go over the chalk array
+        long long kLeftAfterFineRounds = k%sum;
+
+        for(int i=0; i<chalk.size(); i++){
+            if(kLeftAfterFineRounds >= chalk[i]){
+                kLeftAfterFineRounds -= chalk[i];
+            }
+            else return i;
+        }        
+        return 0;
+
     }
 };
