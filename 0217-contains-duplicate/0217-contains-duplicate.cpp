ecd1bc155eff10c33d0389c77c40ca6a7959c65a
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2025-05-14 13:28:08 +0530

    Time: 51 ms (37.03%), Space: 91 MB (14.17%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -4,11 +4,8 @@ public:
         unordered_map<int,int> freqmap;
 
         for(auto& num: nums){
+            if(freqmap[num]) return true;
             freqmap[num]++;
-        }
-
-        for(auto it=freqmap.begin(); it!=freqmap.end(); it++){
-            if(it->second >= 2) return true;
         }
         return false;
     }
