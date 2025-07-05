b0da68737582b73549dfe27db5cce07a2bc63ad1
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2025-07-05 18:22:44 +0530

    Time: 1 ms (84.1%), Space: 27.6 MB (37.29%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -5,43 +5,32 @@
  *     TreeNode *left;
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr, right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
-private:
-    TreeNode* buildTrees(vector<int>& preorder, int preStart, int preEnd, vector<int>& inorder, int inStart, int inEnd, map<int,int> &inorderLoc){
-        //means if there is nothing for preorder of that node, or nothing for inorder of that node then
-        //return from that node
-        if(preStart>preEnd || inStart>inEnd) return nullptr;
-        
-        //as the start of current preorder is always the root
-        TreeNode* root=new TreeNode(preorder[preStart]);
-        
-        //now for this root we have to find it's left nodes and right nodes which will be obtained
-        //from inorder traversal 
-        //eg. inorder: 1,2,{3},4,6  for root 3 left ones are 1,2 and right subtree has 4,6
-        //from inorder we will find index of this root from the map
-        int rootLocInInorder=inorderLoc[root->val];
-        int nodesOnLeft=rootLocInInorder-inStart; 
-                    //1,2 are nodes so 2 nodes (rootLocInInorder is 2 and inorderstarts at 0; 2-0=2)
-        
-        root->left=buildTrees(preorder,preStart+1,preStart+nodesOnLeft,inorder,inStart,rootLocInInorder-1,inorderLoc);
-        root->right=buildTrees(preorder,preStart+nodesOnLeft+1,preEnd,inorder,rootLocInInorder+1,inEnd, inorderLoc);
-        
-        return root;
-        
-    }
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        map<int,int> inorderLoc; //this map stores the index of rach node of inorder
-        
-        for(int i=0;i<inorder.size();i++) 
-            {inorderLoc[inorder[i]]=i;}
-        
-        TreeNode* root=buildTrees(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1,inorderLoc);
+    TreeNode* buildTreeHelper(vector<int>& preorder, int preStart, int preEnd, vector<int>& inorder, int inStart, int inEnd, unordered_map<int,int>& inorderNodeLoc) {
+        if (preStart > preEnd || inStart > inEnd) return nullptr;
+
+        TreeNode *root = new TreeNode(preorder[preStart]);
+        int rootLocInInorder = inorderNodeLoc[root->val];
+        int nodesOnLeft = rootLocInInorder - inStart;
+
+        root->left = buildTreeHelper(preorder, preStart + 1, preStart + nodesOnLeft, inorder, inStart, rootLocInInorder - 1, inorderNodeLoc);
+        root->right = buildTreeHelper(preorder, preStart + nodesOnLeft + 1, preEnd, inorder, rootLocInInorder + 1, inEnd, inorderNodeLoc);
+
         return root;
-            
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        unordered_map<int, int> inorderNodeLoc;
+        for (int i = 0; i < inorder.size(); i++) {
+            inorderNodeLoc[inorder[i]] = i;
+        }
+
+        TreeNode* root = buildTreeHelper(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1, inorderNodeLoc);
+        return root;
     }
 };
