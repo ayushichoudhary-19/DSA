eff501823a3ef43443bde354538d54654897f2a3
Ayushi Choudhary <73214455+ayushichoudhary-19@users.noreply.github.com>
2025-07-05 17:16:07 +0530

    Time: 0 ms (100%), Space: 31.4 MB (16.57%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int getLeftHeight(TreeNode* root){
+        if(root == nullptr){
+            return 0;
+        }
+        int lh = 0;
+
+        TreeNode* temp = root;
+        while(temp){
+            temp = temp ->left;
+            lh ++;
+        }
+        return lh;
+    }
+        int getRightHeight(TreeNode* root){
+        if(root == nullptr){
+            return 0;
+        }
+        int rh = 0;
+
+        TreeNode* temp = root;
+        while(temp){
+            temp = temp ->right;
+            rh ++;
+        }
+        return rh;
+    }
+public:
+    int countNodes(TreeNode* root) {
+        if(!root) return 0;
+
+        int lh = getLeftHeight(root);
+        int rh = getRightHeight(root);
+
+        if(lh == rh){
+            //means this subtree is a CBT
+            return pow(2,lh)-1;
+        }
+
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
