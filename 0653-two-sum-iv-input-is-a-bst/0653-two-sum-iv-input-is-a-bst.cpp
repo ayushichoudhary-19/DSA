7519359c4990a92ead85f37d6be47b2e1a637e8f
Ayushi Choudhary <ayushichoudhary0016@gmail.com>
2025-07-07 18:51:26 +0530

    Time: 0 ms (100%), Space: 37.2 MB (60.34%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -1,38 +1,64 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-private:
-    void findinorder(TreeNode* root, vector<int> &inorder){
-        if(root==NULL) return;
-        
-        findinorder(root->left,inorder);
-        inorder.push_back(root->val);
-        findinorder(root->right,inorder);
+class BSTIterator {
+    stack<TreeNode*> st;
+    bool reverse;
+
+public:
+    BSTIterator(TreeNode* root, bool isReverse) {
+        reverse = isReverse;
+        pushAll(root);
     }
+
+    int next() {
+        TreeNode* node = st.top();
+        st.pop();
+        if (!reverse) {
+            pushAll(node->right);
+        } else {
+            pushAll(node->left);
+        }
+        return node->val;
+    }
+
+    bool hasNext() {
+        return !st.empty();
+    }
+
+private:
+    void pushAll(TreeNode* node) {
+        while (node) {
+            st.push(node);
+            node = reverse ? node->right : node->left;
+        }
+    }
+};
+
+class Solution {
 public:
     bool findTarget(TreeNode* root, int k) {
-        //first find the inorder
-        vector<int> inorder;
-        findinorder(root,inorder);
-        
-        //run normal 2sum function
-        int i=0,j=inorder.size()-1;
-        while(i<j){
-            int currsum = inorder[i]+inorder[j];
-            if(currsum==k) return true;
-            
-            else if(currsum<k) i++;
-            else j--;
+        if (!root) return false;
+
+        BSTIterator l(root, false);
+        BSTIterator r(root, true);
+
+        int i = l.next();
+        int j = r.next();
+
+        while (i < j) {
+            int sum = i + j;
+            if (sum == k) return true;
+            else if (sum < k) {
+                if (l.hasNext())
+                    i = l.next();
+                else
+                    break;
+            } else {
+                if (r.hasNext())
+                    j = r.next();
+                else
+                    break;
+            }
         }
+
         return false;
     }
 };
