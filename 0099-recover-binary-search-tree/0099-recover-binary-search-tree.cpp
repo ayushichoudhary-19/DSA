18a5965ea6b3aed4fce09ebb2b25e9b31863ff2e
Ayushi Choudhary <ayushichoudhary0016@gmail.com>
2025-07-08 00:16:05 +0530

    Time: 0 ms (100%), Space: 60.8 MB (59.05%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -1,35 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-private:
-    void voilationDetection(TreeNode* root, vector<TreeNode*>&voilations,TreeNode* &prev){
-        if(root==nullptr) return;
-        
-        voilationDetection(root->left,voilations,prev);
-        if(prev!=nullptr && prev->val>root->val){
-            voilations.push_back(prev);
-            voilations.push_back(root);
-        }
-        prev=root;
-        voilationDetection(root->right,voilations,prev);
-    }
+
 public:
+    TreeNode *first = nullptr, *middle = nullptr, *last = nullptr, *prev = nullptr;
+    void inorder(TreeNode* root){
+        if(!root) return;
+
+        inorder(root->left);
+        if(prev && prev->val > root->val){
+            if(!first){
+                first = prev;
+                middle = root;
+            }
+            else{
+                last = root;
+            }
+        }
+        prev = root;
+        inorder(root->right);
+    }
+
     void recoverTree(TreeNode* root) {
-        if(root==nullptr) return;
-        //storing voilations in increasing inorder
-        vector<TreeNode*> voilations;
-        TreeNode* prev=nullptr;
-        voilationDetection(root,voilations,prev);
-        if (!voilations.empty()) 
-         swap(voilations[0]->val,voilations[voilations.size()-1]->val);
+        inorder(root);
+
+        if(first && last){
+            swap(first->val, last->val);
+        }
+        else{
+            swap(first->val, middle->val);
+        }
     }
 };
