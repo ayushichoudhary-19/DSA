53abea2d76f5cb6f294e1f96996e5592f045d707
Ayushi Choudhary <ayushichoudhary0016@gmail.com>
2025-07-17 21:17:36 +0530

    Time: 0 ms (100%), Space: 18.5 MB (25.35%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,26 +1,49 @@
 class Solution {
 private:
-    void dfs(vector<vector<int>>& image,vector<vector<int>>& grid, int sr, int sc, int color, int initcolor) {
-        grid[sr][sc] = color;
-        int delrow[4] = {-1, 0, 1, 0};
-        int delcol[4] = {0, 1, 0, -1};
-        for (int i = 0; i < 4; i++) {
-            int nrow = sr + delrow[i];
-            int ncol = sc + delcol[i];
-        if (nrow >= 0 && nrow < grid.size() && ncol >= 0 && ncol < grid[0].size() && image[nrow][ncol] == initcolor && grid[nrow][ncol] != color) {
-            dfs(image, grid, nrow, ncol, color, initcolor);
+    void bfs(int n, int m, vector<vector<int>> &vis, vector<vector<int>> &image,
+             vector<vector<int>> &ans, int row, int col, int newColor, int originalColor) {
+
+        vis[row][col] = 1;
+        ans[row][col] = newColor;
+        queue<pair<int, int>> q;
+        q.push({row, col});
+
+        int drow[] = {-1, 0, 1, 0};
+        int dcol[] = {0, 1, 0, -1};
+
+        while (!q.empty()) {
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+
+            for (int i = 0; i < 4; i++) {
+                int newrow = r + drow[i];
+                int newcol = c + dcol[i];
+
+                if (newrow >= 0 && newrow < n && newcol >= 0 && newcol < m &&
+                    vis[newrow][newcol] == 0 && image[newrow][newcol] == originalColor) {
+
+                    q.push({newrow, newcol});
+                    vis[newrow][newcol] = 1;
+                    ans[newrow][newcol] = newColor;
+                }
+            }
         }
-    }
     }
 
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        // make a copy of the original image
-        vector<vector<int>> grid = image;
+    vector<vector<int>> floodFill(vector<vector<int>> &image,
+                                  int sr, int sc, int newColor) {
+        int n = image.size();
+        int m = image[0].size();
+        int originalColor = image[sr][sc];
 
-        // now traverse over the grid and change the color
-        int initcolor = grid[sr][sc];
-        dfs(image,grid, sr, sc, color, initcolor);
-        return grid;
+        if (originalColor == newColor) return image; 
+
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vector<vector<int>> ans = image;
+
+        bfs(n, m, vis, image, ans, sr, sc, newColor, originalColor);
+        return ans;
     }
 };
