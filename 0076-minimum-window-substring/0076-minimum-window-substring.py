fa3a3891f8869df1b04e7743550b663fd55b2e76
Ayushi Choudhary <ayushichoudhary0016@gmail.com>
2026-05-10 19:59:49 +0530

    Time: 283 ms (14.39%), Space: 19.7 MB (73.94%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,7 +1,7 @@
 from collections import Counter
 
 class Solution:
-    def minWindow(self, s: str, t: str) -> str:
+    def minWindow(self, s: str, t: str):
         m = len(t)
         n = len(s)
 
@@ -11,31 +11,45 @@ class Solution:
         wordCount = Counter(t)
         currCount = Counter()
 
+        need = len(wordCount)
+        have = 0
+
         minlen = float('inf')
         ans = ""
 
         i, j = 0, 0
 
-
         currCount[s[j]] += 1
 
-        while i < n and j < n and i <= j and i >= 0:
+        if s[j] in wordCount and currCount[s[j]] == wordCount[s[j]]:
+            have += 1
 
+        while i < n and j < n and i <= j:
 
             currStr = s[i:j+1]
 
-            if currCount >= wordCount:
+            if have == need:
 
                 currLen = j - i + 1
 
                 if currLen <= minlen:
                     ans = currStr
                     minlen = currLen
+
+                if s[i] in wordCount and currCount[s[i]] == wordCount[s[i]]:
+                    have -= 1
+
                 currCount[s[i]] -= 1
                 i += 1
 
             else:
                 j += 1
-                if j<n: currCount[s[j]] += 1
+
+                if j < n:
+
+                    currCount[s[j]] += 1
+
+                    if s[j] in wordCount and currCount[s[j]] == wordCount[s[j]]:
+                        have += 1
 
         return ans
