af13b1bd24de4bd4eead6283e1a67ac8eb66fe39
Ayushi Choudhary <ayushichoudhary0016@gmail.com>
2026-05-12 00:47:37 +0530

    Time: 0 ms (100%), Space: 19.2 MB (69.63%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -2,19 +2,18 @@ class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         sList = s.split(' ')
         mapping = {}
-        revmapping = {}
         m = len(pattern)
         n = len(sList)
         if m!=n: return False
 
         for i in range(n):
-            if pattern[i] in mapping and mapping[pattern[i]] != sList[i] or sList[i] in revmapping and revmapping[sList[i]] != pattern[i]:
+            if pattern[i] in mapping and mapping[pattern[i]] != sList[i]+'*' or sList[i]+'*' in mapping and mapping[sList[i]+'*'] != pattern[i]:
                         return False
             
             else:
-                mapping[pattern[i]] = sList[i]
-                revmapping[sList[i]] = pattern[i]
-        
+                mapping[pattern[i]] = sList[i]+'*'
+                mapping[sList[i]+'*'] = pattern[i]
+         
 
         return True
 
