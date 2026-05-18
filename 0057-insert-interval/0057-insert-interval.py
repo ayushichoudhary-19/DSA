f80035423ceef2252c969a2f4fd34a1bfdde87b8
Ayushi Choudhary <ayushichoudhary0016@gmail.com>
2026-05-18 15:50:02 +0530

    Time: 3 ms (30.48%), Space: 21.4 MB (26.63%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,35 +1,26 @@
 class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key=lambda x: x[0])
-
-        ans = []
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
         n = len(intervals)
 
+        for i in range(n):
 
-        prev = intervals[0]
-        if n == 1:
-            return [prev]
+            if newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
 
-        for i in range(1,n+1):
-            prevStart = prev[0]
-            prevEnd = prev[1]
-
-            if i<n:
-                currStart = intervals[i][0]
-                currEnd = intervals[i][1]
-
-                if currStart <= prevEnd:
-                    prev = [prevStart, max(currEnd,prevEnd)]
-
-                else:
-                    ans.append(prev)
-                    prev = intervals[i]
-            else:
-                ans.append(prev)
-
-        return ans
+            elif newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
             
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        intervals.append(newInterval)
-        return self.merge(intervals)
-        
+            else:
+                newInterval = [
+                    min(newInterval[0],intervals[i][0]),
+                    max(newInterval[1],intervals[i][1])
+                    ]
+
+
+            
+        res.append(newInterval)
+
+        return res
+
