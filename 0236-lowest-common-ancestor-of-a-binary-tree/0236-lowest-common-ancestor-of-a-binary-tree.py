29e0f013e567e8b0f0f982343b1b7633fd09fa34
Ayushi Choudhary <ayushichoudhary0016@gmail.com>
2026-07-03 18:00:48 +0530

    Time: 121 ms (45.3%), Space: 50.6 MB (32.5%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -6,41 +6,20 @@
 #         self.right = None
 
 class Solution:
-
-    def findPath(self,root,target,path):
-        if not root:
-            return False
-
-        path.append(root)
-
-        if root is target:
-            return True
-
-        if self.findPath(root.left,target,path) or self.findPath(root.right,target,path):
-            return True
-
-        path.pop()
-        return False
-
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+        if not root or root is p or root is q:
+            return root
 
-        # lowest
-        # i love you cutuuuuu
-        # mt preshan ho hojaega sb thik
-        # mzeee krenge weekend is onnnnnn
-        # yayyyyyyyy
-        # nhakr aajau fr pyar krunga apko bott sara
+        left = self.lowestCommonAncestor(root.left,p,q) 
+        right = self.lowestCommonAncestor(root.right,p,q)
 
-        pathA = []
-        pathB = []
-
-        self.findPath(root,p,pathA)
-        self.findPath(root,q,pathB)
-
-        i = 0
-        while i < len(pathA) and i < len(pathB):
-            if pathA[i] != pathB[i]:
-                break
-            i += 1
-            
-        return pathA[i-1]
+        if not left:
+            return right
+        
+        elif not right:
+            return left
+        
+        else:
+            return root
+        
