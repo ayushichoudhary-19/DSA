a75e68981e8e583d6c66cec109384b696b625210
Ayushi Choudhary <ayushichoudhary0016@gmail.com>
2026-07-13 21:15:12 +0530

    Time: 8 ms (28.05%), Space: 22.5 MB (39.81%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,5 +1,5 @@
 class Solution:
-    def solve(self, board: L==t[L==t[str]]) -> None:
+    def solve(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
