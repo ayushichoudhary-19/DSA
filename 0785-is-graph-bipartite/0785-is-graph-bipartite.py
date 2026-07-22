4ab1b17621293b78d640b78ba1f073fff3e4689b
Ayushi Choudhary <ayushichoudhary0016@gmail.com>
2026-07-22 18:24:23 +0530

    Time: 0 ms (100%), Space: 21 MB (10.96%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,28 +1,51 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
+        # colors = {}
+        # total = len(graph)
+        
+        # for u in range(total):
+        #     if u not in colors:
+        #         colors[u] = 'A'
+        #     for v in graph[u]:
+        #         if v not in colors:
+        #             if colors[u] == 'A':
+        #                 colors[v] = 'B'
+        #             else:
+        #                 colors[v] = 'A'
+        #         else:
+        #             if colors[v] == colors[u]:
+        #                 return False
+
+        # return True
+
+              
+            
         colors = {}
         total = len(graph)
-
+  
         def dfs(node):
-            for nei in graph[node]:
-                if nei not in colors:
+            for v in graph[node]:
+                if v not in colors:
                     if colors[node] == 'A':
-                        colors[nei] = 'B'
+                        colors[v] = 'B'
                     else:
-                        colors[nei] = 'A'
+                        colors[v] = 'A'
 
-                    if not dfs(nei):
+                    if not dfs(v):
                         return False
 
-                elif colors[nei] == colors[node]:
-                    return False
+                else:
+                    if colors[v] == colors[node]:
+                        return False
 
             return True
+            
 
         for u in range(total):
             if u not in colors:
                 colors[u] = 'A'
                 if not dfs(u):
                     return False
+        
 
         return True
