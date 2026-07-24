d6011b3a8869ca1dbe57f323fbd2ef2c681b2aa4
Ayushi Choudhary <ayushichoudhary0016@gmail.com>
2026-07-24 23:39:21 +0530

    Time: 51 ms (91.36%), Space: 38.1 MB (24.68%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -16,32 +16,32 @@ class Solution:
                 newy = y + dely[k]
 
                 if newx >= 0 and newx < m and newy>=0 and newy < n and grid[newx][newy] == 1:
-                    grid[newx][newy] = 2
+                    grid[newx][newy] = -1
                     dfs(newx,newy)
         
         #traverse the 4 corners and do not count them at the end
         i,j = 0,0
         for i in range(m):
             if grid[i][j] == 1:
-                grid[i][j] = 2
+                grid[i][j] = -1
                 dfs(i,j)
 
         j = n-1
         for i in range(m):
             if grid[i][j] == 1:
-                grid[i][j] = 2
+                grid[i][j] = -1
                 dfs(i,j)
 
         i = 0
         for j in range(n):
             if grid[i][j] == 1:
-                grid[i][j] = 2
+                grid[i][j] = -1
                 dfs(i,j)
 
         i = m-1
         for j in range(n):
             if grid[i][j] == 1:
-                grid[i][j] = 2
+                grid[i][j] = -1
                 dfs(i,j)
 
         # count all the 1'es which are not hashed that is not connected to boundry 1
