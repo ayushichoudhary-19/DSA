d7c690cd9841f3928d433f3e82fe90f7e21e435c
Ayushi Choudhary <ayushichoudhary0016@gmail.com>
2026-07-24 23:39:06 +0530

    Time: 60 ms (77.47%), Space: 37.9 MB (25.58%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -0,0 +1,55 @@
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+
+        delx = [0,1,0,-1]
+        dely = [-1,0,1,0]
+
+        count = 0
+
+        def dfs(x,y):
+            newx = x
+            newy = y
+            for k in range(4):
+                newx = x + delx[k]
+                newy = y + dely[k]
+
+                if newx >= 0 and newx < m and newy>=0 and newy < n and grid[newx][newy] == 1:
+                    grid[newx][newy] = 2
+                    dfs(newx,newy)
+        
+        #traverse the 4 corners and do not count them at the end
+        i,j = 0,0
+        for i in range(m):
+            if grid[i][j] == 1:
+                grid[i][j] = 2
+                dfs(i,j)
+
+        j = n-1
+        for i in range(m):
+            if grid[i][j] == 1:
+                grid[i][j] = 2
+                dfs(i,j)
+
+        i = 0
+        for j in range(n):
+            if grid[i][j] == 1:
+                grid[i][j] = 2
+                dfs(i,j)
+
+        i = m-1
+        for j in range(n):
+            if grid[i][j] == 1:
+                grid[i][j] = 2
+                dfs(i,j)
+
+        # count all the 1'es which are not hashed that is not connected to boundry 1
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    count += 1
+
+        return count
+
