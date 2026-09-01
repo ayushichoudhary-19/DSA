a76f05128daae1f6606dae3f9785e9c0695c5835
Ayushi Choudhary <ayushichoudhary0016@gmail.com>
2026-09-01 14:40:02 +0530

    Time: 3 ms (84.21%), Space: 22.4 MB (60.11%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -4,54 +4,52 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         
+        # if it is a boundary component, don't change else change
+
+        # mark the boundary component diferently to make sure they never change
+        # change rest of the board
+
         m = len(board)
         n = len(board[0])
 
         delx = [0,1,0,-1]
         dely = [-1,0,1,0]
 
-        def dfs(x,y):
-            newx = x
-            newy = y
+        def dfs(i,j):
+            board[i][j] = '#'
             for k in range(4):
-                newx = x + delx[k]
-                newy = y + dely[k]
+                newx = i+delx[k]
+                newy = j+dely[k]
 
-                if newx >= 0 and newx < m and newy>=0 and newy < n and board[newx][newy] == 'O':
-                    board[newx][newy] = '#'
-                    dfs(newx,newy)
+                if newx >= 0 and newx < m and newy >= 0 and newy <n:
+                    if board[newx][newy] == 'O':
+                        dfs(newx,newy)
 
-        #traverse the 4 corners
-        i,j = 0,0
-        for i in range(m):
-            if board[i][j] == 'O':
-                board[i][j] = '#'
-                dfs(i,j)
-
-        j = n-1
-        for i in range(m):
-            if board[i][j] == 'O':
-                board[i][j] = '#'
-                dfs(i,j)
-
-        i = 0
+        # top boundary
         for j in range(n):
-            if board[i][j] == 'O':
-                board[i][j] = '#'
-                dfs(i,j)
-
-        i = m-1
+            if board[0][j] == 'O':
+                dfs(0,j)
+            
+        # bottom bondary
         for j in range(n):
-            if board[i][j] == 'O':
-                board[i][j] = '#'
-                dfs(i,j)
+            if board[m-1][j] == 'O':
+                dfs(m-1,j)
 
+        # left boundary
+        for i in range(m):
+            if board[i][0] == 'O':
+                dfs(i,0)
 
-        #fill the whole board with correct values
+        # right boundary
+        for i in range(m):
+            if board[i][n-1] == 'O':
+                dfs(i,n-1)
+
+        # mark flagged ones as 'O' rest 'X'
 
         for i in range(m):
             for j in range(n):
                 if board[i][j] == '#':
                     board[i][j] = 'O'
                 else:
-                    board[i][j] = 'X' 
+                    board[i][j] = 'X'
