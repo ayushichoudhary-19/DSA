e3bf003cb404ccfd23db53e655f033e5776e0504
Ayushi Choudhary <ayushichoudhary0016@gmail.com>
2026-09-04 10:05:35 +0530

    Time: 7 ms (43.61%), Space: 20.3 MB (93.11%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,35 +1,43 @@
-from collections import defaultdict
+from collections import defaultdict, deque
+# For a directed graph, cycle detection with BFS is done using indegree.
+
+# Idea
+# Count how many incoming edges each node has.
+# Put all nodes with indegree 0 into a queue.
+# Remove them one by one.
+# Every time you remove a node, decrease the indegree of its neighbors.
+# If a neighbor becomes 0, push it into the queue.
+# Count how many nodes you processed.
+# If you processed all nodes, there is no cycle.
+# If some nodes remain, they are part of a cycle.
+
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj = defaultdict(list)
 
-        for u, v in prerequisites:
-            adj[u].append(v)
+        adjlist = defaultdict(list)
+        indegree = [0] * numCourses
 
-        visited = set()
-        path = set()
+        for course, prerequisite in prerequisites:
+            adjlist[prerequisite].append(course)
+            indegree[course] += 1
 
-        def dfs(node):
-            if node in path:
-                return True      # cycle found
-
-            if node in visited:
-                return False     # already processed
-
-            visited.add(node)
-            path.add(node)
-
-            for nei in adj[node]:
-                if dfs(nei):
-                    return True
-
-            path.remove(node)
-            return False
+        q = deque()
 
         for course in range(numCourses):
-            if course not in visited:
-                if dfs(course):
-                    return False   # cannot finish
+            if indegree[course] == 0:
+                q.append(course)
 
-        return True
+        completed = 0
+
+        while q:
+            node = q.popleft()
+            for neig in adjlist[node]:
+                indegree[neig] -= 1
+
+                if indegree[neig] == 0:
+                    q.append(neig)
+
+            completed += 1
+
+        return completed == numCourses
