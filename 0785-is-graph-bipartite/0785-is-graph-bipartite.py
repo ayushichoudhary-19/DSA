514dda04728036a373886660fca1a5352f5d5a40
Ayushi Choudhary <ayushichoudhary0016@gmail.com>
2026-09-06 21:46:44 +0530

    Time: 2 ms (39.37%), Space: 20.6 MB (30.23%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,51 +1,28 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        # colors = {}
-        # total = len(graph)
-        
-        # for u in range(total):
-        #     if u not in colors:
-        #         colors[u] = 'A'
-        #     for v in graph[u]:
-        #         if v not in colors:
-        #             if colors[u] == 'A':
-        #                 colors[v] = 'B'
-        #             else:
-        #                 colors[v] = 'A'
-        #         else:
-        #             if colors[v] == colors[u]:
-        #                 return False
-
-        # return True
-
-              
-            
         colors = {}
         total = len(graph)
-  
+
         def dfs(node):
-            for v in graph[node]:
-                if v not in colors:
+            for nei in graph[node]:
+                if nei not in colors:
                     if colors[node] == 'A':
-                        colors[v] = 'B'
+                        colors[nei] = 'B'
                     else:
-                        colors[v] = 'A'
-
-                    if not dfs(v):
+                        colors[nei] = 'A'
+                    
+                    if not dfs(nei):
                         return False
-
-                else:
-                    if colors[v] == colors[node]:
-                        return False
+                
+                elif colors[nei] == colors[node]:
+                    return False
 
             return True
-            
-
-        for u in range(total):
-            if u not in colors:
-                colors[u] = 'A'
-                if not dfs(u):
+         
+        for node in range(total):
+            if node not in colors:
+                colors[node] = 'A'
+                if not dfs(node):
                     return False
-        
 
         return True
