adf2aef055f2183dfdd900622eaf5fd5df3deef0
Ayushi Choudhary <ayushichoudhary0016@gmail.com>
2026-09-07 13:36:01 +0530

    Time: 47 ms (43.06%), Space: 21.4 MB (9.34%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
@@ -1,46 +1,43 @@
+from collections import defaultdict
 class Solution:
     def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
         visited = set()
+        adj = defaultdict(list)
 
-        adj = collections.defaultdict(list)
-
-        # prep the graph
         for u,v in edges:
             adj[u].append(v)
             adj[v].append(u)
 
-        
+        component = []
         count = 0
 
-        component = []
+        def checkconnected(component):
+            # a connected component is one in which every node is connected to every other node, so if there are n nodes, then each has n-1 neighbours
+            size = len(component)
+            for node in component:
+                totalNeighbors = len(adj[node])
+                if totalNeighbors != size - 1:
+                    return False
+            
+            return True
+
 
         def dfs(node):
             visited.add(node)
+
             component.append(node)
-            for nei in adj[node]:
-                if nei not in visited:
-                    dfs(nei)
+            for neig in adj[node]:
+                if neig not in visited:
+                    dfs(neig)
 
         for node in range(n):
             if node not in visited:
-
-                component = []
                 dfs(node)
-            
-                # now we have a new component, we check if it is a connected component
-                totalNodesInComponent = len(component)
-                
-                # for it to be a complete component each node should have n-1  i.e.:  totalNodesInComponent-1 neighbours
-
-
-                complete = True
-
-                for u in component:
-                    if len(adj[u]) != totalNodesInComponent-1:
-                        complete = False
-                        break
-                    
-                if complete:
+                # here we are returned when one new component is done travering
+                # we check if it is a connected component
+                if checkconnected(component):
                     count += 1
+                
+                component = []
 
         return count
