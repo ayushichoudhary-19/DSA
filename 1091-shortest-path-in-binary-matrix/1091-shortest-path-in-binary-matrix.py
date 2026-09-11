461d143d18851c4008e55057ba1289a4e3b8ed31
Ayushi Choudhary <ayushichoudhary0016@gmail.com>
2026-09-11 12:29:18 +0530

    Time: 127 ms (77.43%), Space: 19.9 MB (61.45%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,14 +1,14 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
 
-        if grid[0][0] == 1:
+        n = len(grid)
+        if grid[0][0] == 1 or grid[n-1][n-1] == 1:
             return -1
         q = deque()
-        n = len(grid)
         q.append((1,0,0))
 
-        visited = set()
-        visited.add((0,0))
+        # changing grid to mark visited - not interview recommended but ask if u can
+        grid[0][0] = 1
 
         delx = [1,1,0,-1,-1,-1,0,1]
         dely = [0,1,1,1,0,-1,-1,-1]
@@ -23,8 +23,8 @@ class Solution:
                 newrow = row + delx[k]
                 newcol = col + dely[k]
 
-                if 0 <= newrow < n and 0 <= newcol < n and grid[newrow][newcol] == 0 and (newrow, newcol) not in visited:
-                    visited.add((newrow,newcol))
+                if 0 <= newrow < n and 0 <= newcol < n and grid[newrow][newcol] == 0:
+                    grid[newrow][newcol] = 1
                     q.append((total+1,newrow,newcol))
 
             
