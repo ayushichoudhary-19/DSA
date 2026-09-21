94b34042e64c861a9039e466644697b2dd755cfe
Ayushi Choudhary <ayushichoudhary0016@gmail.com>
2026-09-22 00:09:56 +0530

    Time: 15 ms (41.39%), Space: 23.9 MB (27.03%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,18 +1,21 @@
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
         hashmap = {}
-
         for word in strs:
             freq = [0]*26
-            for letter in word:
-                index = ord(letter) - ord('a')
-                freq[index] += 1
+
+            for char in word:
+                idx = ord(char) - ord('a')
+                freq[idx] += 1
             
-            #Python lists cannot be used as dictionary keys, but tuples can.
+            
             tup = tuple(freq)
             if tup in hashmap:
                 hashmap[tup].append(word)
+
             else:
                 hashmap[tup] = [word]
-
+            
         return list(hashmap.values())
+
+        
